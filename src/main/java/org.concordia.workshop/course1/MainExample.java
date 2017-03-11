1a4735bf86515a25b3ae07b8665b4d9d8b24911a
Charles Cao <caotaotao@gmail.com>
2017-03-11 01:57:36 -0500

    add hello world

---
+++ src/main/java/org.concordia.workshop/course1/MainExample.java
@@ -0,0 +1,12 @@
+package org.concordia.workshop.course1;
+
+/**
+ * Created by caoc on 3/11/17.
+ * Copyright (c) 2015 Service ECVictor Inc. All rights reserved.
+ */
+public class MainExample {
+    public static void main(String[] args) {
+        String helloWorld = "Hello World!";
+        System.out.println(helloWorld);
+    }
+}
