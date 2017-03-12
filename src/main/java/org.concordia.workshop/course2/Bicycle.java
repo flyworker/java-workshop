2b4a968d77a5c55c99c2280ec356413017608503
Charles Cao <caotaotao@gmail.com>
2017-03-12 12:44:56 -0400

    course 2

---
+++ src/main/java/org.concordia.workshop/course2/Bicycle.java
@@ -0,0 +1,47 @@
+package org.concordia.workshop.course2;
+
+/**
+ * Created by caoc on 3/12/17.
+ * Copyright (c) 2015 Service ECVictor Inc. All rights reserved.
+ */
+public class Bicycle {
+
+    private int cadence;
+    private int gear;
+    private int speed;
+
+    Bicycle(int startCadence, int startSpeed, int startGear) {
+        gear = startGear;
+        cadence = startCadence;
+        speed = startSpeed;
+    }
+
+    public int getCadence() {
+        return cadence;
+    }
+
+    public void setCadence(int newValue) {
+        cadence = newValue;
+    }
+
+    public int getGear() {
+        return gear;
+    }
+
+    public void setGear(int newValue) {
+        gear = newValue;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void applyBrake(int decrement) {
+        speed -= decrement;
+    }
+
+    public void speedUp(int increment) {
+        speed += increment;
+    }
+}
+
---
+++ src/main/java/org.concordia.workshop/course2/CrunchifyStaticExample.java
@@ -0,0 +1,24 @@
+package org.concordia.workshop.course2;
+
+/**
+ * Created by caoc on 3/12/17.
+ * Copyright (c) 2015 Service ECVictor Inc. All rights reserved.
+ */
+public class CrunchifyStaticExample {
+
+    public static void main(String[] args) {
+        CrunchifyStaticDeclaration.setTestValue(5);
+
+        // non-private static variables can be accessed with class name
+        CrunchifyStaticDeclaration.testString = "\nAssigning testString a value";
+        CrunchifyStaticDeclaration csd = new CrunchifyStaticDeclaration();
+        System.out.println(csd.getTestString());
+
+        // class and instance static variables are same
+        System.out.print("\nCheck if Class and Instance Static Variables are same:  ");
+        System.out.println(CrunchifyStaticDeclaration.testString == csd.testString);
+        System.out.println("Why? Because: CrunchifyStaticDeclaration.testString == csd.testString");
+    }
+}
+
+
