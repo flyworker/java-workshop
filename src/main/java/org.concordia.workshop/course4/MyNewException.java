b0a4f18946c3222facd5fa2583842d1f880b5363
ccao <caotaotao@gmail.com>
2017-03-25 03:45:50 -0400

    add slides for j2ee

--- src/main/java/org.concordia.workshop/course4/MyNewException.java
+++ src/main/java/org.concordia.workshop/course4/MyNewException.java
@@ -4,6 +4,8 @@ package org.concordia.workshop.course4;
  * Created by ccao on 3/19/17.
  */
 public class MyNewException extends RuntimeException {
+    int anInt;
+    float aFloat;
 
     public MyNewException(){
         super();
