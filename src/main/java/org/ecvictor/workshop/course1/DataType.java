b08661940534096ea74d7c0f31ddb61c022d20c2
ccao <ccao@ecvictor.com>
2017-08-12 12:48:58 -0400

    add DataType

---
+++ src/main/java/org/ecvictor/workshop/course1/DataType.java
@@ -0,0 +1,38 @@
+package org.ecvictor.workshop.course1;
+
+/**
+ * Created by ccao on 2017-08-12.
+ */
+public class DataType {
+    /*
+    Test for hello world.
+     */
+    public static void main(String[] args) {
+
+//        int count, tripleCounter;
+//
+//        count = 5;
+//
+//        tripleCounter = count * 3;
+//        double divideCounter = count / 2.0;
+//        int remainCounter = count % 2;
+
+        int secondPerDay = 24 * 60 * 60;
+        int august = 31 * secondPerDay;
+        int september = 30 * secondPerDay;
+        System.out.println(august + september);
+
+
+//        System.out.println(tripleCounter);
+//
+//        double l = 1.1;
+//        System.out.println(l);
+//
+//        boolean b = false;
+//        System.out.println(b);
+//
+//        String c = "a";
+//        System.out.println(c);
+
+    }
+}
