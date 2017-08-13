15b502016b3072162df25f7f8ac806caefc889ac
Chandler Zhu <chandler.zhu@chandlertechnology.com>
2017-08-12 22:13:58 -0400

    Course 2 exercise

---
+++ src/main/java/org/ecvictor/workshop/exercises/ControlFlow1.java
@@ -0,0 +1,10 @@
+package org.ecvictor.workshop.exercises;
+
+import java.util.Random;
+
+/**
+ * Step 1: Code the logic to generate a random array with length 20 (Random rand = new Random(); int a = rand.nextInt();) and print them separated by coma
+ * Step 2: Extract all elements with value > 25 and print
+ */
+public class ControlFlow1 {
+}
