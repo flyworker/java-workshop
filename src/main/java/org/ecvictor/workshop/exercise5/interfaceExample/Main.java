91dc6c05f115528b24f8efa89fbb367248a2ea2b
ccao <ccao@ecvictor.com>
2017-08-26 11:57:46 -0400

    update

--- src/main/java/org/ecvictor/workshop/exercise5/interfaceExample/Main.java
+++ src/main/java/org/ecvictor/workshop/exercise5/interfaceExample/Main.java
@@ -9,6 +9,8 @@ public class Main {
     public static void main(String[] args) {
         Dog dog = new Dog();
         dog.bark();
+
+
 //        Hound hound=new Hound();
 //        hound.bark();
         Whale whale=new Whale() ;
