570d947831a1c32c607546dacb38f0f34057431d
Chandler Zhu <zhuqianca@gmail.com>
2017-08-26 16:16:51 -0400

    Java Exception&Collection

--- src/main/java/org/ecvictor/workshop/exercises/FlowControl2.java
+++ src/main/java/org/ecvictor/workshop/exercises/FlowControl2.java
@@ -7,7 +7,7 @@ import java.util.Random;
  * Step 2: Find pairs of elements(2 elements close to each other) that the sum value is 25, print them line by line
  * Step 3: Find pairs of elements(2 elements may or may NOT close to each other) that the sum value is 25, print them line by line
  */
-public class ControlFlow2 {
+public class FlowControl2 {
     public static void main(String args[]){
         int[] arr = {6,7,1,19,22,3,67,12,9,16,13,88,19,32,25,5,0};
 
