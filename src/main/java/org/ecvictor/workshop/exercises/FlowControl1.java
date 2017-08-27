06690883e025348f17050c2eb94c06f95d2bb8ce
Chandler Zhu <chandler.zhu@chandlertechnology.com>
2017-08-26 22:00:31 -0400

    Flow Control Solution

--- src/main/java/org/ecvictor/workshop/exercises/FlowControl1.java
+++ src/main/java/org/ecvictor/workshop/exercises/FlowControl1.java
@@ -7,4 +7,16 @@ import java.util.Random;
  * Step 2: Extract all elements with value > 25 and print
  */
 public class FlowControl1 {
+    public static void main(String args[]) {
+        int[] random = new int[20];
+        Random rand = new Random();
+        for (int i = 0; i < random.length; i++) {
+            random[i] = rand.nextInt(50);
+        }
+        for(int i = 0; i < random.length; i++) {
+            if(random[i] > 25) {
+                System.out.print(random[i]+",");
+            }
+        }
+    }
 }
--- src/main/java/org/ecvictor/workshop/exercises/FlowControl2.java
+++ src/main/java/org/ecvictor/workshop/exercises/FlowControl2.java
@@ -1,5 +1,6 @@
 package org.ecvictor.workshop.exercises;
 
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -9,10 +10,48 @@ import java.util.Random;
  */
 public class FlowControl2 {
     public static void main(String args[]){
-        int[] arr = {6,7,1,19,22,3,67,12,9,16,13,88,19,32,25,5,0};
+        int[] arr = {6, 7, 1, 19, 22, 3, 67, 12, 9, 16, 13, 88, 19, 32, 25, 5, 0};
 
-        int pre = arr[0];
-        for(int i=1;i<arr.length;i++){
+        // Step 2
+        int prev = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] + prev == 25) {
+                System.out.println(String.valueOf(prev) + ',' + String.valueOf(arr[i]));
+            }
+            prev = arr[i];
+        }
+
+        System.out.println();
+        //Step 3, solution 1 with 2 for loops
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == 25) {
+                    System.out.println(String.valueOf(arr[i]) + ',' + String.valueOf(arr[j]));
+                }
+
+            }
+        }
+
+        System.out.println();
+        // Step 3, solution 2 with two pointers
+        Arrays.sort(arr);
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right && left < arr.length && right < arr.length) {
+
+            if (arr[left] + arr[right] == 25) {
+                System.out.println(String.valueOf(arr[left]) + ',' + String.valueOf(arr[right]));
+                if (arr[left] == arr[left + 1]) {
+                    left++;
+                } else {
+                    right--;
+                }
+
+            } else if (arr[left] + arr[right] < 25) {
+                left++;
+            } else if (arr[left] + arr[right] > 25) {
+                right--;
+            }
 
         }
     }
