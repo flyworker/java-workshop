6e1f89c855952279edbc0e8249aa4f9d8d08318a
Chandler Zhu <chandler.zhu@chandlertechnology.com>
2017-09-16 12:03:18 -0400

    Java training 2, preparation

---
+++ src/main/java/com/ecvlearning/java/exercises/Calculation1.java
@@ -0,0 +1,16 @@
+package com.ecvlearning.java.exercises;
+
+/**
+ * Created by caoc on 3/12/17.
+ * Copyright (c) 2015 Service ECVictor Inc. All rights reserved.
+ */
+/*
+/*
+(Summation of a series) Write a program that displays the result of
+				1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9.
+*/
+public class Calculation1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/ecvlearning/java/exercises/Calculation2.java
@@ -0,0 +1,17 @@
+package com.ecvlearning.java.exercises;
+
+/*
+Write a program to solve linear equation:
+y = ax + b
+a and b are constants
+pick three values for x and calculate the result
+*/
+public class Calculation2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int x,y;
+
+    }
+}
